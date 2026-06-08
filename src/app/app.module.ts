654c5c5a6ca8935a0969c08ec4548e838ac4a000
Padmalaya Meher <meherpadmalaya185@gmail.com>
2026-06-08 15:57:41 +0700

    attendance file is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AttendanceDashboardComponent } from './attendance-dashboard/attendance-dashboard.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AttendanceDashboardComponent } from './attendance-dashboard/attendance-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    HomeComponent,
     AttendanceDashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatButtonModule,
-    MatTableModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule,
-    MatChipsModule,
-    MatFormFieldModule
+    CommonModule,
+    RouterModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
