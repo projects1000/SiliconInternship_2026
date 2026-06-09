c377b880ff246b303dd44c712f3398936dc04cdb
Jagannath Padhi <padhijaga200@gmail.com>
2026-06-09 12:09:45 +0530

    Remove unnecessary blank line in app.module.ts
    Commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
