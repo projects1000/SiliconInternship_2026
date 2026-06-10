0a9364180b7dd04c8f9404b34c1dd85aac603443
Gayatri <mishrarama49@gmail.com>
2026-06-10 22:00:47 +0530

    final commit

--- src/app/grp-c/member1/attendance/attendance.module.ts
+++ src/app/grp-c/member1/attendance/attendance.module.ts
@@ -3,6 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home/home.component';
 import { AttendanceRoutingModule } from './attendance-routing.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -10,7 +26,24 @@ import { AttendanceRoutingModule } from './attendance-routing.module';
   ],
   imports: [
     CommonModule,
-    AttendanceRoutingModule
+    AttendanceRoutingModule,
+    MatSidenavModule,
+    MatTreeModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    CdkTreeModule,
+    FormsModule,
+    MatChipsModule
+    
   ]
 })
 export class AttendanceModule { }
