a63c55b879a514379237612e21edd336e71e318c
mamabisoi <mamabisoi71@gmail.com>
2026-06-12 09:23:59 +0530

    fixed error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 import { ProductManagementComponent } from './product-management/product-management.component';
 import { ProductListComponent } from './product-management/product-list/product-list.component';
 import { CustomerComponent } from './product-management/customer/customer.component';
 import { BillingComponent } from './product-management/billing/billing.component';
+
+// Angular Material (ONLY keep if you are actually using them)
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
+
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
@@ -22,13 +37,29 @@ import { ToastrModule } from 'ngx-toastr';
     ProductManagementComponent,
     ProductListComponent,
     CustomerComponent,
-    BillingComponent,
+    BillingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+
+    // Material modules
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
+    MatChipsModule,
+    MatDividerModule,
+
+    // Toastr
     ToastrModule.forRoot({
       timeOut: 3000,
       positionClass: 'toast-top-right',
