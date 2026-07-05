293da9ebd7ebdbf0e1489a045b7a6eee0c8ac096
Omkar <cse.24bcsh12@silicon.ac.in>
2026-07-05 19:23:26 +0530

    done

--- src/app/grp-b/member5/product-list/product-list.component.ts
+++ src/app/grp-b/member5/product-list/product-list.component.ts
@@ -22,126 +22,126 @@ export class ProductListComponent {
       category: 'Cold Drink',
       price: 40,
       stock: true,
-      image: 'assets/products/coke.jpg'
+      image: 'assets/Omkar_products/coke.jpg'
     },
-
+  
     {
       id: 2,
       name: 'Pepsi',
       category: 'Cold Drink',
       price: 40,
       stock: true,
-      image: 'assets/products/pepsi.jpg'
+      image: 'assets/Omkar_products/pepsi.jpg'
     },
-
+  
     {
       id: 3,
       name: 'Sprite',
       category: 'Cold Drink',
       price: 40,
       stock: false,
-      image: 'assets/products/sprite.jpg'
+      image: 'assets/Omkar_products/sprite.jpg'
     },
-
+  
     {
       id: 4,
       name: 'Monster Energy',
       category: 'Energy Drink',
       price: 150,
       stock: true,
-      image: 'assets/products/monster.jpg'
+      image: 'assets/Omkar_products/monster.jpg'
     },
-
+  
     {
       id: 5,
       name: 'Red Bull',
       category: 'Energy Drink',
       price: 125,
       stock: true,
-      image: 'assets/products/redbull.jpg'
+      image: 'assets/Omkar_products/redbull.jpg'
     },
-
+  
     {
       id: 6,
       name: 'Sting',
       category: 'Energy Drink',
       price: 25,
       stock: false,
-      image: 'assets/products/sting.jpg'
+      image: 'assets/Omkar_products/sting.jpg'
     },
-
+  
     {
       id: 7,
       name: 'Dairy Milk',
       category: 'Chocolate',
       price: 50,
       stock: true,
-      image: 'assets/products/dairymilk.jpg'
+      image: 'assets/Omkar_products/dairymilk.jpg'
     },
-
+  
     {
       id: 8,
       name: 'KitKat',
       category: 'Chocolate',
       price: 20,
       stock: true,
-      image: 'assets/products/kitkat.jpg'
+      image: 'assets/Omkar_products/kitkat.jpg'
     },
-
+  
     {
       id: 9,
       name: 'Snickers',
       category: 'Chocolate',
       price: 45,
       stock: false,
-      image: 'assets/products/snickers.jpg'
+      image: 'assets/Omkar_products/snickers.jpg'
     },
-
+  
     {
       id: 10,
       name: 'Protein Shake',
       category: 'Protein',
       price: 180,
       stock: true,
-      image: 'assets/products/protein.jpg'
+      image: 'assets/Omkar_products/protein.jpg'
     },
-
+  
     {
       id: 11,
       name: 'Veg Sandwich',
       category: 'Veg',
       price: 90,
       stock: true,
-      image: 'assets/products/veg.jpg'
+      image: 'assets/Omkar_products/veg.jpg'
     },
-
+  
     {
       id: 12,
       name: 'Chicken Burger',
       category: 'Non Veg',
       price: 150,
       stock: true,
-      image: 'assets/products/chicken.jpg'
+      image: 'assets/Omkar_products/chicken.jpg'
     },
-
+  
     {
       id: 13,
       name: 'Coke Zero',
       category: 'Zero Sugar',
       price: 55,
       stock: true,
-      image: 'assets/products/cokezero.jpg'
+      image: 'assets/Omkar_products/cokezero.jpg'
     },
-
+  
     {
       id: 14,
       name: 'Pepsi Black',
       category: 'Zero Sugar',
       price: 55,
       stock: false,
-      image: 'assets/products/pepsiblack.jpg'
+      image: 'assets/Omkar_products/pepsiblack.jpg'
     }
-
+  
   ];
 
   get filteredProducts() {
